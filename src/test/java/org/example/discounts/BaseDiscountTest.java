57133009d27b8f55a7221ce83c6cdb0b94b6e948
Dan Saa <dansaa8@protonmail.com>
2023-09-28 15:11:17 +0200

    Chained discounts return expected value, test successful.

--- src/test/java/org/example/discounts/BaseDiscountTest.java
+++ src/test/java/org/example/discounts/BaseDiscountTest.java
@@ -17,9 +17,28 @@ class BaseDiscountTest {
         double appliedDiscount = expected -
                 new QuantityDiscount().apply(p) +
                 new MilkDiscount().apply(p) +
-                new QuantityDiscount().apply(p);
+                new FridayDiscount("Friday").apply(p);
 
         assertThat(appliedDiscount).isEqualTo(expected);
+    }
+
+    @Test
+    void productWithSeveralDiscountsAppliedReturnCombinedDiscount() {
+        var combinedDiscounts = new MilkDiscount(new QuantityDiscount(new FridayDiscount("Friday")));
+
+
+        Product p = new Product("Milk", 19, 7);
+
+        double productTotal =  p.price() * p.quantity();
+
+        double expected =
+                productTotal * 0.05 // <- milk
+                               + 70 // <- quantity
+                + productTotal * 0.10; // friday
+
+
+
+        assertEquals(expected, combinedDiscounts.apply(p));
     }
 
 }
