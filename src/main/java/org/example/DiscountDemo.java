d33679646069c848410019a62cc6e5899996fe9e
Dan Saa <dansaa8@protonmail.com>
2023-09-28 15:18:39 +0200

    Create a simple demo using the different discounts in various combinations.

--- src/main/java/org/example/DiscountDemo.java
+++ src/main/java/org/example/DiscountDemo.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import org.example.discounts.FridayDiscount;
+import org.example.discounts.MilkDiscount;
+import org.example.discounts.QuantityDiscount;
+
 public class DiscountDemo {
     public static void main(String[] args) {
+        Product p1 = new Product("Cheese", 40, 3);
+        Product p2 = new Product("Milk", 20, 5);
+        Product p3 = new Product("Beer", 17, 6);
 
+        var discount = new FridayDiscount("Friday");
+        var combined1 = new MilkDiscount(new FridayDiscount(new QuantityDiscount(), "Wednesday"));
+        var combined2 = new QuantityDiscount(new FridayDiscount("Monday"));
+
+        System.out.println("Discounts for cheese: " + discount.apply(p1));
+        System.out.println("Discounts for milk: " + combined1.apply(p2));
+        System.out.println("Discounts for beer: " + combined2.apply(p3));
     }
 }
